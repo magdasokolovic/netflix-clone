f72548ffd0c4a486d506cb474b56c1b8f7a42893
magdasokolovic <magda.sokolovic@gmail.com>
2021-06-30 11:57:52 +0200

    minor styling change

--- ui/src/App.js
+++ ui/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="container">
 
         <Row title="Netflix Originals" />
         <Row title="Trending Now" />
--- ui/src/components/Row.js
+++ ui/src/components/Row.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import data from '../mockData.js/data.json'
 
 export default function Row({title}) {
   
   const [movies, setMovies] = useState([])
+  
+  useEffect(()=>{
 
+  }, [])
+
+  console.log(data)
   return (
     <div>
       <h2>{title}</h2>
+      
     </div>
   )
     
