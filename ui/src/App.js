a68cf0e307ca3b88bca5c625eeb53ce913acb3e6
juliasckr <jule.s@live.de>
2021-07-01 14:53:54 +0200

    add navbar

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,20 +1,25 @@
-
-import { BrowserRouter, Switch, Route} from "react-router-dom";
-import Row from './components/Row'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Row from "./components/Row";
 import Home from "./pages/Home";
 import Serie from "./pages/Serie.overview";
 import Season from "./pages/Season.overview";
 import Episode from "./pages/Episode.overview";
-import './sass/main.scss'
+import "./sass/main.scss";
+import Navbar from "./components/Navbar";
 
 // import data from './mockData.js/data.json'
 
 function App() {
-
   return (
     <BrowserRouter>
       <div className="App">
-
+        <Navbar />
+        {/* image needs to be exchanged later with the header */}
+        <img
+          src="https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80"
+          alt="cinema"
+          className="hero"
+        />
         <Row title="Netflix Originals" />
         <Row title="Trending Now" />
 
