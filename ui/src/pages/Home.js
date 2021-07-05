15fa6b988e306fdcb9ca2d0c330caeb672542d2c
juliasckr <jule.s@live.de>
2021-07-05 11:28:23 +0200

    add to top functionality & update search function

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,15 +1,11 @@
 import React from "react";
+import Banner from "../components/Banner";
 import Row from "../components/Row";
 
 function Home() {
   return (
     <div>
-      {/* image needs to be exchanged later with the header */}
-      <img
-        src="https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80"
-        alt="cinema"
-        className="hero"
-      />
+      <Banner />
       <Row title="Netflix Originals" />
       <Row title="NETFLIX ORIGINALS" isLargeRow />
       <Row title="Trending Now" />
