8d3b91a7d21e43f40bbaf318e54b24dbdc741460
Chi Tran <khanhchi.tran13051@gmail.com>
2021-07-05 15:19:57 +0200

    add more films/categories API

--- api/src/controllers/controller.js
+++ api/src/controllers/controller.js
@@ -2,8 +2,10 @@ const { Series, Films } = require("../models");
 const axios = require("axios");
 const api_key = "a815bbb393e7f79ac4b0c66acd437aa6";
 const base_url = "https://api.themoviedb.org/3";
-const latest_url = `${base_url}/movie/latest?api_key=${api_key}&language=en-US
-`;
+const latest_url = `${base_url}/movie/latest?api_key=${api_key}&language=en-US`;
+const popular_url = `${base_url}/movie/popular?api_key=${api_key}&language=en-US&page=1`;
+const toprated_url = `${base_url}/movie/top_rated?api_key=${api_key}&language=en-US&page=1`;
+const upcoming_url = `${base_url}/movie/upcoming?api_key=${api_key}&language=en-US&page=1`;
 const getAllSeries = (req, res) => {
   Series.find({}, (err, data) => {
     if (err) {
@@ -21,7 +23,7 @@ const getAllFilms = (req, res) => {
   });
 };
 const getAllLatest = async (req, res) => {
-  console.log("haha");
+  // console.log("haha");
   try {
     const response = await axios.get(latest_url);
     const data = response.data;
@@ -30,4 +32,41 @@ const getAllLatest = async (req, res) => {
     res.status(500).json(error);
   }
 };
-module.exports = { getAllSeries, getAllFilms, getAllLatest };
+const getAllPopular = async (req, res) => {
+  // console.log("haha");
+  try {
+    const response = await axios.get(popular_url);
+    const data = response.data;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+const getAllTopRated = async (req, res) => {
+  // console.log("haha");
+  try {
+    const response = await axios.get(toprated_url);
+    const data = response.data;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+const getAllUpcoming = async (req, res) => {
+  // console.log("haha");
+  try {
+    const response = await axios.get(upcoming_url);
+    const data = response.data;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+module.exports = {
+  getAllSeries,
+  getAllFilms,
+  getAllLatest,
+  getAllPopular,
+  getAllTopRated,
+  getAllUpcoming
+};
