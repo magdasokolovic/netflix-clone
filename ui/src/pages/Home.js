11acd60c155e1fa5cc1ff00a70f66327efe1604b
Chi Tran <khanhchi.tran13051@gmail.com>
2021-07-06 13:03:30 +0200

    added Loading component

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import Row from "../components/Row";
 import Header from "../components/Header";
-
+import Loading from "../components/Loading";
 function Home() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    //here will fetch the movies and here will update movies with "setMovies"
+    fetch("http://localhost:5000/api/series")
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setLoading(false);
+        setMovies(result);
+      });
+  }, []);
   return (
     <div>
+      {loading && <Loading />}
       <Header />
       <Banner />
-      <Row title="Netflix Originals" />
-      <Row title="NETFLIX ORIGINALS" isLargeRow />
-      <Row title="Trending Now" />
-      <Row title="Top rated" />
+      <Row movies={movies} title="Netflix Originals" />
+      <Row movies={movies} title="NETFLIX ORIGINALS" isLargeRow />
+      <Row movies={movies} title="Trending Now" />
+      <Row movies={movies} title="Top rated" />
     </div>
   );
 }
